813a161b509a87c560449c2f58a0d927965f248d
elianajuradx <Thinkingoutloud1>
2021-02-18 20:17:41 -0500

    * sales-system.csprojAssemblyReference.cache:
    * Program.cs: Sales total amount calculated by concatenation

--- sales-system/Program.cs
+++ sales-system/Program.cs
@@ -6,7 +6,32 @@ namespace sales_system
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Seller a1 = new Seller();
+            a1.name = "Jacob";
+            a1.age = 33;
+
+            Seller a2 = new Seller();
+            a2.name = "marian";
+            a2.age = 27;
+
+            Seller a3 = new Seller();
+            a3.name = "Harry";
+            a3.age = 27;
+
+            Product b1 = new Product();
+            b1.name = "Shampoo";
+            b1.price = 22000;
+
+            Product b2 = new Product();
+            b2.name = "Eye Shadow";
+            b2.price = 259000;
+
+            Product b3 = new Product();
+            b3.name = "Red Lipstick";
+            b3.price = 25000;
+
+            Console.Write(b2.price + b1.price + b3.price);
+
         }
     }
 }
